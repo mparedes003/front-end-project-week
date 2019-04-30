db46c1ee49fa162fb5c4dd1fc0c4b6c3c2dd6c11
mparedes003 <onnabugeishadev@gmail.com>
2019-04-30 16:17:20 -0700

    Edit GET and POST to use deployed backend

--- lambda_times_sprint/src/App.js
+++ lambda_times_sprint/src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
-import './components/NotesList.css';
-import './components/NoteForm.css';
-import './components/Note.css';
+import React, { Component } from "react";
+import "./App.css";
+import "./components/NotesList.css";
+import "./components/NoteForm.css";
+import "./components/Note.css";
 
-import { Route, NavLink, withRouter } from 'react-router-dom';
-import axios from 'axios';
-import { Auth0Lock } from 'auth0-lock';
-import auth0 from 'auth0-js';
+import { Route, NavLink, withRouter } from "react-router-dom";
+import axios from "axios";
+import { Auth0Lock } from "auth0-lock";
+import auth0 from "auth0-js";
 
-import NotesList from './components/NotesList';
-import NoteForm from './components/NoteForm';
-import Note from './components/Note';
+import NotesList from "./components/NotesList";
+import NoteForm from "./components/NoteForm";
+import Note from "./components/Note";
 
 var lock = new Auth0Lock(
   process.env.REACT_APP_CLIENT_ID,
@@ -44,8 +44,8 @@ webAuth.parseHash((err, authResult) => {
 class App extends Component {
   state = {
     tags: [],
-    title: '',
-    textBody: '',
+    title: "",
+    textBody: ""
   };
 
   componentDidMount() {
@@ -55,7 +55,9 @@ class App extends Component {
   // allows us to get all the notes data from the API
   fetchNotes = () => {
     // axios.get('https://killer-notes.herokuapp.com/note/get/all')
-    axios.get('http://localhost:9900/api/notes')
+    // axios.get('http://localhost:9900/api/notes')
+    axios
+      .get("https://lambdnotes.herokuapp.com/api/notes")
       .then(response => {
         // console.log(response);
         this.setState({ tags: response.data });
@@ -63,27 +65,31 @@ class App extends Component {
       .catch(err => {
         console.log(err);
       });
-  }
+  };
 
   // allows us to add title and textBody info for new note created on state
   handleInputChange = event => {
     this.setState({ [event.target.name]: event.target.value });
-  }
+  };
 
   // allows us to create a new note and post it to the API
   handleAddNewNote = event => {
-    console.log('firing')
+    console.log("firing");
 
     const note = { title: this.state.title, textBody: this.state.textBody };
 
     // axios.post('https://killer-notes.herokuapp.com/note/create', note)
-    axios.post('http://localhost:9900/api/notes', note)
-      .then(response => this.setState({
-        note: response.data,
-        title: response.data.title,
-        textBody: response.data.textBody
-      }))
-  }
+    // axios.post('http://localhost:9900/api/notes', note)
+    axios
+      .post("https://lambdnotes.herokuapp.com/api/notes", note)
+      .then(response =>
+        this.setState({
+          note: response.data,
+          title: response.data.title,
+          textBody: response.data.textBody
+        })
+      );
+  };
 
   render() {
     // console.log("PROCESS: ", process.env);
@@ -96,14 +102,14 @@ class App extends Component {
               <li>
                 <NavLink exact to="/notes" activeClassName="activeNavButton">
                   View Your Notes
-            </NavLink>
+                </NavLink>
               </li>
               <li>
                 <NavLink to="/note-form" activeClassName="activeNavButton">
                   +Create New Note
-            </NavLink></li>
-              <li className="logout"
-                onClick={this.logout}>
+                </NavLink>
+              </li>
+              <li className="logout" onClick={this.logout}>
                 Log Out
               </li>
             </ul>
@@ -121,7 +127,6 @@ class App extends Component {
               render={props => (
                 <NoteForm
                   {...props}
-
                   title={this.state.title}
                   textBody={this.state.textBody}
                   handleAddNewNote={this.handleAddNewNote}
@@ -129,29 +134,27 @@ class App extends Component {
                 />
               )}
             />
-            <Route path="/notes/:id" render={props =>
-              <Note
-                {...props}
-                fetchNotes={this.fetchNotes}
-              />
-            }
+            <Route
+              path="/notes/:id"
+              render={props => <Note {...props} fetchNotes={this.fetchNotes} />}
             />
-
           </div>
-
         </div>
       );
     } else {
       return (
         <div className="please-login">
           <h1 className="access-notes">Please Log In to Access Notes</h1>
-          <div className="login"
-            onClick={function () {
+          <div
+            className="login"
+            onClick={function() {
               lock.show();
             }}
-          >Log In</div>
+          >
+            Log In
+          </div>
         </div>
-      )
+      );
     }
   }
 
@@ -159,17 +162,16 @@ class App extends Component {
   isAuthenticated() {
     // Check whether the current time is past the
     // Access Token's expiry time
-    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    let expiresAt = JSON.parse(localStorage.getItem("expires_at"));
     return new Date().getTime() < expiresAt;
   }
 
   // remove token data from localStorage
   logout() {
     // Clear Access Token and ID Token from local storage
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('expires_at');
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("expires_at");
     window.location.reload();
-
   }
 }
 
